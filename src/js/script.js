2b933f8af40560d20a0aaca980974f138f882381
EgorKancir <kancyregor@gmail.com>
2024-05-12 16:11:57 +0300

    Egor section

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+});
